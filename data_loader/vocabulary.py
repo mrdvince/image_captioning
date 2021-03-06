c4ba20de0c261ec417f0854bcdc1812c2970db1b
Droid021 <vincivenv@gmail.com>
2021-03-06 11:12:35 +0300

    voabulary

---
+++ data_loader/vocabulary.py
@@ -0,0 +1,81 @@
+import nltk
+import pickle
+import os.path
+from pycocotools.coco import COCO
+from collections import Counter
+
+
+class Vocabulary(object):
+    def __init__(
+        self,
+        vocab_threshold,
+        vocab_file='./vocab.pkl',
+        start_word='<start>',
+        end_word='<end>',
+        unk_word='<unk>',
+        annotations_file='/mnt/c/Users/eggy/Downloads/Compressed/coco_sample/coco_sample/annotations/train_sample.json',
+        vocab_from_file=False,
+    ):
+        self.vocab_threshold = vocab_threshold
+        self.vocab_file = vocab_file
+        self.start_word = start_word
+        self.end_word = end_word
+        self.unk_word = unk_word
+        self.annotations_file = annotations_file
+        self.vocab_from_file = vocab_from_file
+        self.get_vocab()
+
+    def get_vocab(self):
+        """Load the vocabulary from file OR build the vocabulary from scratch."""
+        if os.path.exists(self.vocab_file) & self.vocab_from_file:
+            with open(self.vocab_file, 'rb') as f:
+                vocab = pickle.load(f)
+                self.word2idx = vocab.word2idx
+                self.idx2word = vocab.idx2word
+            print('Vocabulary loaded from vocab.pkl file!')
+        else:
+            self.build_vocab()
+            with open(self.vocab_file, 'wb') as f:
+                pickle.dump(self, f)
+
+    def build_vocab(self):
+        self.init_vocab()
+        self.add_word(self.start_word)
+        self.add_word(self.end_word)
+        self.add_word(self.unk_word)
+        self.add_captions()
+
+    def init_vocab(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
+
+    def add_word(self, word):
+        if not word in self.word2idx:
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
+
+    def add_captions(self):
+        coco = COCO(self.annotations_file)
+        counter = Counter()
+        ids = coco.anns.keys()
+        for i, id in enumerate(ids):
+            caption = str(coco.anns[id]['caption'])
+            tokens = nltk.tokenize.word_tokenize(caption.lower())
+            counter.update(tokens)
+            
+            if i % 100000 == 0:
+                print("[%d/%d] Tokenizing captions..." % (i, len(ids)))
+        words = [word for word, cnt in counter.items() if cnt >= self.vocab_threshold]
+
+        for i, word in enumerate(words):
+            self.add_word(word)
+        
+    def __call__(self, word):
+        if not word in self.word2idx:
+            return self.word2idx[self.unk_word]
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.word2idx)
